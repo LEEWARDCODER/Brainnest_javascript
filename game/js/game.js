abf6671d9a24543dce3ca37b6b78b734a828d853
LEEWARD <64976499+LEEWARDCODER@users.noreply.github.com>
2023-04-29 08:54:38 +0100

    separate  functions

--- game/js/game.js
+++ game/js/game.js
@@ -1,4 +1,43 @@
-game();
+
+function computerPlay() {
+  // generate a random number between 0 and 3
+  const randomNumber = Math.random() * 3;
+  let result;
+
+  if (randomNumber >= 0 && randomNumber < 1) result = "Rock";
+  else if (randomNumber >= 1 && randomNumber < 2) result = "Paper";
+  else if (randomNumber >= 2 && randomNumber < 3) result = "Scissors";
+  else throw exception("Random number must be between 0 and 3");
+
+  return result;
+}
+
+
+function getUserSelection() {
+  let playerSelection;
+  do {
+    let input = getUserInput();
+    // validat
+    if (input === "" || input === null) continue;
+
+    playerSelection = input.toLocaleLowerCase().trim();;
+    playerSelection = playerSelection.replace(
+      playerSelection[0],
+      playerSelection[0].toUpperCase()
+    );
+  } while (!["Rock", "Paper", "Scissor"].includes(playerSelection));
+
+  return playerSelection;
+}
+
+function getUserInput() {
+ const input = prompt(
+     "Input your choice from either Rock Paper and Scissor for 5 times, invalid inputs will be ignored",
+      ""
+    );
+  return input;
+}
+
 function game() {
   let count = 0;
   let endWinner = "Nobody (draw)";
@@ -19,24 +58,7 @@ function game() {
   console.log(`winner is ${endWinner} `);
 }
 
-function getUserSelection() {
-  let playerSelection;
-  do {
-    const input = prompt(
-     "Input your choice from either Rock Paper and Scissor for 5 times, invalid inputs will be ignored",
-      ""
-    );
-    if (input === "" || input === null) continue;
 
-    playerSelection = input.toLocaleLowerCase().trim();;
-    playerSelection = playerSelection.replace(
-      playerSelection[0],
-      playerSelection[0].toUpperCase()
-    );
-  } while (!["Rock", "Paper", "Scissor"].includes(playerSelection));
-
-  return playerSelection;
-}
 
 function playRound(player, computer) {
   let result = "draw";
@@ -57,15 +79,4 @@ function playRound(player, computer) {
   return report;
 }
 
-function computerPlay() {
-  // generate a random number between 0 and 3
-  const randomNumber = Math.random() * 3;
-  let result;
-
-  if (randomNumber >= 0 && randomNumber < 1) result = "Rock";
-  else if (randomNumber >= 1 && randomNumber < 2) result = "Paper";
-  else if (randomNumber >= 2 && randomNumber < 3) result = "Scissors";
-  else throw exception("Random number must be between 0 and 3");
-
-  return result;
-}
+game();
